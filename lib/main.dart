3d36b2509b2df9ea3be7a8c26e5afaa76c8290bb
yotsufe <yotsufe@gmail.com>
2021-01-15 20:40:34 +0900

    Scaffold ・appBar ・body ・backgroundColor ・image

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Center(child: Text('Hello World'))));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('Flutter Base'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Image(
+          image: NetworkImage(
+              'https://i2.wp.com/www.oakridge.in/wp-content/uploads/2020/02/Sample-jpg-image-500kb.jpg'),
+        ),
+      ),
+    ),
+  );
 }
