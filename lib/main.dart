59c074324d217cf1e93910ed79d330162fc92cbd
yotsufe <yotsufe@gmail.com>
2021-01-24 15:24:17 +0900

    Add Dice Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,104 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_base/dice.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MaterialApp(
+      home: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.yellow[300],
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('images/purgatory.png'),
+    return Scaffold(
+      backgroundColor: Colors.yellow[300],
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/purgatory.png'),
+            ),
+            Text(
+              '煉獄 杏寿郎',
+              style: TextStyle(
+                color: Colors.red[700],
+                fontFamily: 'Sawarabi',
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
               ),
-              Text(
-                '煉獄 杏寿郎',
-                style: TextStyle(
-                  color: Colors.red[700],
-                  fontFamily: 'Sawarabi',
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.bold,
+            ),
+            Text(
+              '炎柱',
+              style: TextStyle(
+                color: Colors.red[500],
+                fontFamily: 'Sawarabi',
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 4,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+              child: Divider(
+                color: Colors.red.shade100,
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 24.0,
+              ),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.red[300],
+                  size: 20.0,
+                ),
+                title: Text(
+                  '090 1234 5678',
                 ),
               ),
-              Text(
-                '炎柱',
-                style: TextStyle(
-                  color: Colors.red[500],
-                  fontFamily: 'Sawarabi',
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 4,
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 24.0,
+              ),
+              child: ListTile(
+                leading: Icon(
+                  Icons.mail,
+                  color: Colors.red[300],
+                  size: 20.0,
+                ),
+                title: Text(
+                  'abcde12345@abcde.com',
                 ),
               ),
-              SizedBox(
-                height: 20.0,
-                child: Divider(
-                  color: Colors.red.shade100,
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 24.0,
-                ),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
-                    color: Colors.red[300],
-                    size: 20.0,
+            ),
+            RaisedButton(
+              child: const Text('Dice'),
+              color: Colors.orange,
+              textColor: Colors.white,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return Dice();
+                    },
                   ),
-                  title: Text(
-                    '090 1234 5678',
-                  ),
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 24.0,
-                ),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.mail,
-                    color: Colors.red[300],
-                    size: 20.0,
-                  ),
-                  title: Text(
-                    'abcde12345@abcde.com',
-                  ),
-                ),
-              ),
-            ],
-          ),
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
