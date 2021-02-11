43a03218cd5a406050f064087961fe20dff44435
yotsufe <yotsufe@gmail.com>
2021-02-11 20:20:41 +0900

    Change to final

--- lib/bmicalculator/inputpage.dart
+++ lib/bmicalculator/inputpage.dart
@@ -60,7 +60,7 @@ class _InputPageState extends State<InputPage> {
 class ReusableCard extends StatelessWidget {
   ReusableCard({this.colour});
 
-  Color colour;
+  final Color colour;
 
   @override
   Widget build(BuildContext context) {
